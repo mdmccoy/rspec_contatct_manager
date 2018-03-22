10cf6463388a99a57fc117e5304bb26a8470124d
mdmccoy <mdmccoy@gmail.com>
2018-03-22 11:48:17 -0400

    All specs passing.

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without first_name" do
+    person = Person.new(first_name: nil, last_name:"Doe")
+    expect(person).not_to be_valid
+  end
+
+  it "is invalid without last_name" do
+    person =  Person.new(last_name: nil, first_name: "Jane")
+    expect(person).not_to be_valid
+  end
 end
