4b1bff78f7837464000b0f3d1bab66fb954f6d69
Matt McCoy <mdmccoy@gmail.com>
2018-03-22 17:29:31 -0400

    PhoneNumber model tests.

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:person) do
+    Person.create(first_name:"Alice", last_name:"Smith")
+  end
+
+  let(:phone_number) do
+    PhoneNumber.new(person: person, number:"1112223333")
+  end
+
+  it "is valid" do
+    expect(phone_number).to be_valid
+  end
+
+  it "is invalid without a number" do
+    phone_number.number =  nil
+    expect(phone_number).to_not be_valid
+  end
+
 end
