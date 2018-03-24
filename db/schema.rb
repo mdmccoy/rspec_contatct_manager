08aa3f7e2fcb6f6e05cb7ad0228798c2c3962e59
Matt McCoy <mdmccoy@gmail.com>
2018-03-24 12:59:03 -0400

    Scaffold companies.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180324131526) do
+ActiveRecord::Schema.define(version: 20180324165611) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "email_addresses", force: :cascade do |t|
     t.string "address"
---
+++ spec/features/phone_number_view_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'the phone_number view' do
+  let(:person) { Person.create!(first_name: 'John', last_name: 'Doe') }
+  let(:phone_number) { PhoneNumber.create!(number:'1112223333', person_id:person.id)}
+
+  before(:each) { visit phone_number_path(phone_number)}
+
+  it "back button takes you to person page" do
+    click_link 'Back'
+    expect(current_path).to eq(person_path(person))
+  end
+end
