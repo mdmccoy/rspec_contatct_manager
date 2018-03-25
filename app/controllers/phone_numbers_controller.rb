ab974273d4938cb877cb1457c710474614440e69
Matt McCoy <mdmccoy@gmail.com>
2018-03-25 14:11:03 -0400

    Refactor models, show pages.

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -1,5 +1,5 @@
 class PhoneNumbersController < ApplicationController
-  before_action :set_phone_number, only: [:show, :edit, :update, :destroy]
+  before_action :find_resource, only: [:edit, :update, :destroy]
 
   # GET /phone_numbers/new
   def new
@@ -12,7 +12,6 @@ class PhoneNumbersController < ApplicationController
   end
 
   # POST /phone_numbers
-  # POST /phone_numbers.json
   def create
     @phone_number = PhoneNumber.new(phone_number_params)
 
@@ -20,45 +19,33 @@ class PhoneNumbersController < ApplicationController
     respond_to do |format|
       if @phone_number.save
         format.html { redirect_to @phone_number.contact, notice: 'Phone number was successfully created.' }
-        format.json { render :show, status: :created, location: @phone_number }
       else
         format.html { render :new }
-        format.json { render json: @phone_number.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /phone_numbers/1
-  # PATCH/PUT /phone_numbers/1.json
   def update
     respond_to do |format|
       if @phone_number.update(phone_number_params)
         format.html { redirect_to @phone_number.contact, notice: 'Phone number was successfully updated.' }
-        format.json { render :show, status: :ok, location: @phone_number }
       else
         format.html { render :edit }
-        format.json { render json: @phone_number.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /phone_numbers/1
-  # DELETE /phone_numbers/1.json
   def destroy
     contact = @phone_number.contact
     @phone_number.destroy
     respond_to do |format|
       format.html { redirect_to contact, notice: 'Phone number was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_phone_number
-      @phone_number = PhoneNumber.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def phone_number_params
       params.require(:phone_number).permit(:number, :contact_id, :contact_type)
