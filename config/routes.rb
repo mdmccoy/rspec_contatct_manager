ee6a014872819e5e64e061c35dde47e10d5ac529
Matt McCoy <mdmccoy@gmail.com>
2018-03-25 13:00:20 -0400

    Refactor email address controller, views, specs, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :companies
-  resources :email_addresses
+  resources :email_addresses, except: [:index,:show]
   resources :people
   resources :phone_numbers, except: [:index,:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
